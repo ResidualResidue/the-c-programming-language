603ed583b2df9061c7591257425e9f050f6900d7
Michael Foley <michaelfoley1999@gmail.com>
2025-05-02 23:37:24 -0400

    Added header to far/celc table

--- 1-2-while.c
+++ 1-2-while.c
@@ -3,12 +3,14 @@
 float get_celcius(float);
 
 int main(){
-    printf("Farenheight to Celcius table: \n");
+    printf("Farenheight to Celcius table: \n\n");
     float farenheight = 0;
     float celcius = 0;
+    printf("Far  |  Celc\n");
+    printf("============\n");
     while(farenheight <= 300){
         celcius = get_celcius(farenheight);
-        printf("%3.0f %7.1f\n", farenheight, celcius);
+        printf("%3.0f  | %5.1f\n", farenheight, celcius);
         farenheight+=20;
     }
     return 0;
