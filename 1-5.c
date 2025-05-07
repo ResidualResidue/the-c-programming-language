e9493a8d7012326baa59ee88beb2fa36414d9d44
Michael Foley <michaelfoley1999@gmail.com>
2025-05-06 23:15:29 -0400

    Define statements - symbolic constants

--- 1-5.c
+++ 1-5.c
@@ -1,4 +1,13 @@
 #include <stdio.h>
+
+/*
+Symbolic constants -- define a constant to be a particular string of characters.
+After defining these, any occurance of the name will be replaced by corresponding replacement text
+*/
+#define LOWER 0
+#define UPPER 300
+#define STEP 20
+
 float get_celcius(int);
 
 
@@ -7,7 +16,7 @@ int main(){
     float celcius;
     printf("Farenheight |     Celcius\n");
     printf("============ ============\n");
-    for(farenheight=0; farenheight<=300; farenheight+=20){
+    for(farenheight=LOWER; farenheight<=UPPER; farenheight+=STEP){
         celcius = get_celcius(farenheight);
         printf("%11d | %11.1f\n", farenheight, celcius);
     }
